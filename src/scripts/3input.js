b47cbe9a0fb5970cf92781481eff9ca113e37ce4
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2019-12-05 08:36:41 +0100

    Trying to solve third puzzle

---
+++ src/scripts/3input.js
@@ -0,0 +1,4 @@
+const wire1 = ["R8", "U5", "L5", "D3"];
+const wire2 = ["U7", "R6", "D4", "L4"];
+
+export { wire1, wire2 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,63 @@
-import data from "./2input";
+import { wire1, wire2 } from "./3input";
 
-const inputCopy = data.slice();
-let input;
+let arr = {
+  R: 0,
+  L: 0,
+  D: 0,
+  U: 0
+};
 
-/* FIRST PART OF THE PUZZLE */
+let arr2 = {
+  R: 0,
+  L: 0,
+  D: 0,
+  U: 0
+};
 
-function calculateState(i, noun, verb) {
-  i[1] = noun;
-  i[2] = verb;
-  for (let p = 0; p < i.length; p += 4) {
-    i[p] == 99
-      ? true
-      : i[p] == 1
-      ? (i[i[p + 3]] = i[i[p + 1]] + i[i[p + 2]])
-      : i[p] == 2
-      ? (i[i[p + 3]] = i[i[p + 1]] * i[i[p + 2]])
-      : (p = p + 4);
-  }
-  return i[0];
-}
+function Calculate(w1, w2) {
+  let dir1;
+  let dir2;
+  let val1;
+  let val2;
+  let p = 0;
+  while (p <= w1.length) {
+    dir1 = w1[p].charAt(0);
+    val1 = parseInt(w1[p].substr(1));
+    dir2 = w2[p].charAt(0);
+    val2 = parseInt(w2[p].substr(1));
 
-/* SECOND PART OF THE PUZZLE */
+    if (dir1 == "R") {
+      arr.R += val1;
+    } else if (dir1 == "L") {
+      arr.L += val1;
+    } else if (dir1 == "D") {
+      arr.D += val1;
+    } else if (dir1 == "U") {
+      arr.U += val1;
+    }
 
-for (let k = 0; k <= 99; k++) {
-  for (let j = 0; j <= 99; j++) {
-    input = inputCopy.slice();
-    if (calculateState(input, k, j) === 19690720) {
-      console.log(k, j);
-      console.log("Result: ", 100 * k + j);
+    if (dir2 == "R") {
+      arr2.R += val2;
+    } else if (dir2 == "L") {
+      arr2.L += val2;
+    } else if (dir2 == "D") {
+      arr2.D += val2;
+    } else if (dir2 == "U") {
+      arr2.U += val2;
+    }
+    if (
+      arr.R == arr2.R &&
+      arr.L == arr2.L &&
+      arr.D == arr2.D &&
+      arr.U == arr2.U
+    ) {
+      console.log(obj1);
+    } else {
+      console.log(arr);
+      console.log(arr2);
+      p++;
     }
   }
 }
+
+Calculate(wire1, wire2);
