c9b10f2cd3e8be2ce28f8416e1c5da8ea00cea53
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2019-12-16 17:49:02 +0100

    Miserable tries and failures on day 4

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
-//const input = [128392, 643281];
-const input = [122222, 643281];
+const input = [128392, 643281];
+//const input = [111122, 643281];
 
 function findPasswordNumber(input) {
   let lowEnd = input[0];
@@ -16,15 +16,34 @@ function findPasswordNumber(input) {
         return parseInt(t);
       });
 
-    for (let k = 0; k < digits.length - 1; k++) {
+    for (let k = 0; k <= digits.length - 1; k++) {
       let j = k + 1;
       if (digits[k] <= digits[k + 1]) {
+        answer.push(digits[k]);
+      } else if (k == digits.length - 1) {
         answer.push(digits[k]);
       }
     }
 
     if (answer.length == 6) {
-      goodPasswords.push(answer.join(""));
+      let doubles = false;
+      let triples = false;
+      for (let q = 0; q <= answer.length - 2; q++) {
+        if (
+          q < answer.length - 2 &&
+          answer[q] == answer[q + 1] &&
+          answer[q] != answer[q + 2]
+        ) {
+          doubles = true;
+          if (q < answer.length - 2 && answer[q] == answer[q + 2]) {
+            triples = true;
+          }
+        }
+      }
+
+      if (doubles == true) {
+        goodPasswords.push(answer.join(""));
+      }
     }
   }
 
